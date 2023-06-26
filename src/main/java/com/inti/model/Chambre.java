43dbbdbd400b05675b5ca4e8e3ed89747d7d52c6
Titou82Bi <inacio.baptiste@gmail.com>
2023-06-26 15:40:53 +0200

    Cruds

--- src/main/java/com/inti/model/Chambre.java
+++ src/main/java/com/inti/model/Chambre.java
@@ -1,13 +1,15 @@
 package com.inti.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.springframework.lang.NonNull;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
