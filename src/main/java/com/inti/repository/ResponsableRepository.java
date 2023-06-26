b65e7a862cc0a6ef7b00b61ba26401a5b57af802
Gurvaan <Gurvan@DESKTOP-S8E7TRO>
2023-06-26 13:44:53 +0200

    Ajout classe ResponsableController

--- src/main/java/com/inti/repository/ResponsableRepository.java
+++ src/main/java/com/inti/repository/ResponsableRepository.java
@@ -2,8 +2,8 @@ package com.inti.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.inti.model.Client;
+import com.inti.model.Responsable;
 
 @Repository
-public interface ResponsableRepository extends JpaRepository<Client, Integer> {
+public interface ResponsableRepository extends JpaRepository<Responsable, Integer> {
 }
